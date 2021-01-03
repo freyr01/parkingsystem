b2b7b28da0bc261ca209cc1d47ae683c8301395e
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-01-03 19:23:09 +0100

    Write IT for park a car and exit a car

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,7 +1,9 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.service.ParkingService;
@@ -13,7 +15,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -51,15 +56,34 @@ public class ParkingDataBaseIT {
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+     
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        int nextAvailableSlot = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+        int ticketParkingSpot = ticket.getParkingSpot().getId();
+        
+        assertEquals("ABCDEF", ticket.getVehicleRegNumber());
+        assertNotEquals(null, ticket.getInTime());
+        assertEquals(null, ticket.getOutTime());
+        assertNotEquals(nextAvailableSlot, ticketParkingSpot);
     }
 
     @Test
     public void testParkingLotExit(){
         testParkingACar();
+   
+        //Wait 2sec before exiting to prevent bad out time exception
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    	
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        
+        assertNotEquals(null, ticket.getOutTime());
     }
 
 }
