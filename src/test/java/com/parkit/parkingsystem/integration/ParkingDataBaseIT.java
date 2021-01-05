336497bbb9eade555a348c5aa94321c6709d1652
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-01-05 19:40:21 +0100

    Add parking spot test

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,5 +1,6 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.DBConstants;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
@@ -8,6 +9,9 @@ import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,11 +23,18 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
-
+	private static final Logger logger = LogManager.getLogger("ParkingDataBaseIT");
     private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
@@ -62,29 +73,43 @@ public class ParkingDataBaseIT {
         int nextAvailableSlot = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
         int ticketParkingSpot = ticket.getParkingSpot().getId();
         
-        assertEquals("ABCDEF", ticket.getVehicleRegNumber());
-        assertNotEquals(null, ticket.getInTime());
-        assertEquals(null, ticket.getOutTime());
-        assertNotEquals(nextAvailableSlot, ticketParkingSpot);
+        assertEquals("ABCDEF", ticket.getVehicleRegNumber());	//Check if registration number of the ticket is the same as expected
+        assertNotNull(ticket.getInTime());
+        assertNull(ticket.getOutTime());
+        assertNotEquals(nextAvailableSlot, ticketParkingSpot); // Check if next available slot and current ticket parking spot is not the same
     }
 
-    @Test
+    @Test()
     public void testParkingLotExit(){
         testParkingACar();
    
         //Wait 2sec before exiting to prevent bad out time exception
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(100);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
     	
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
         Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        int parkingId = ticket.getParkingSpot().getId();
+        Connection con = null;
+        boolean parkingSpotIsAvailable = false;
+        try {
+	        con = dataBaseTestConfig.getConnection();
+	        String request = "select p.available from parking p where PARKING_NUMBER = ? and AVAILABLE = 1";
+	        PreparedStatement ps = con.prepareStatement(request);
+	        ps.setInt(1, parkingId);
+	        ResultSet rs = ps.executeQuery();
+	        parkingSpotIsAvailable = rs.first();
+        } catch (SQLException | ClassNotFoundException e) {
+        	logger.error("Error fetching available slot in IT", e);
+        }   finally {
+            dataBaseTestConfig.closeConnection(con);
+        }
         
-        //TODO Verify next available slot
+        assertTrue(parkingSpotIsAvailable);
         assertNotNull(ticket.getOutTime());
     }
 
